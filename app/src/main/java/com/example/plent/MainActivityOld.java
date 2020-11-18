79b6f02bcd8278990b2b395d78e228ba8fcb3579
radjsh <arissa140100@yahoo.com.sg>
2020-11-18 11:45:59 +0800

    First Commit

---
+++ app/src/main/java/com/example/plent/MainActivityOld.java
@@ -0,0 +1,15 @@
+package com.example.plent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class MainActivityOld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_activity);
+    }
+}
---
+++ app/src/main/java/com/example/plent/myActivities/FindEventsActivity.java
@@ -0,0 +1,108 @@
+package com.example.plent.myActivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+
+import com.example.plent.R;
+
+import java.util.ArrayList;
+
+public class FindEventsActivity extends AppCompatActivity {
+
+    ArrayList<Integer> images = new ArrayList<>();
+    ArrayList<Object> imagesId = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.find_events_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ImageView fr_image_1 = findViewById(R.id.fr_image_1);
+        ImageView fr_image_2 = findViewById(R.id.fr_image_2);
+        ImageView fr_image_3 = findViewById(R.id.fr_image_3);
+        ImageView fr_image_4 = findViewById(R.id.fr_image_4);
+        ImageView fr_image_5 = findViewById(R.id.fr_image_5);
+        ImageView fr_image_6 = findViewById(R.id.fr_image_6);
+
+        images.add(R.drawable.athleticsposter);
+        images.add(R.drawable.bands_poster);
+        images.add(R.drawable.judo_poster);
+        images.add(R.drawable.football_poster);
+        images.add(R.drawable.robotics_poster);
+        images.add(R.drawable.tennis_poster);
+
+        fr_image_1.setImageResource(images.get(0));
+        fr_image_2.setImageResource(images.get(1));
+        fr_image_3.setImageResource(images.get(2));
+        fr_image_4.setImageResource(images.get(3));
+        fr_image_5.setImageResource(images.get(4));
+        fr_image_6.setImageResource(images.get(5));
+
+        CardView fifth_row_events_card_view = findViewById(R.id.fifth_row_events_card_view);
+        TextView header = fifth_row_events_card_view.findViewById(R.id.cluster_header);
+        header.setText("Fifth Row Activities");
+//        TextView cluster_header = findViewById(R.id.cluster_header);
+//        cluster_header.setText(R.string.fifth_row_activities);
+
+        CardView industry_talks_card_view = findViewById(R.id.industry_talks_card_view);
+        TextView header1 = industry_talks_card_view.findViewById(R.id.cluster_header);
+        header1.setText("Industry Talks");
+
+        CardView upcoming_events_card_view = findViewById(R.id.upcoming_events_card_view);
+        TextView header2 = upcoming_events_card_view.findViewById(R.id.cluster_header);
+        header2.setText("Upcoming Events");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.my_information) {
+            return true;
+        }
+
+        if (id == R.id.my_calender) {
+            Intent intent = new Intent(FindEventsActivity.this, CalendarActivity.class);
+            startActivity(intent);
+        }
+
+        if (id == R.id.find_events_and_activities) {
+            Intent intent = new Intent(FindEventsActivity.this, EventActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void redirectToEventsPage(View view){
+        Intent intent = new Intent(FindEventsActivity.this, EventActivity.class);
+        startActivity(intent);
+
+    }
+
+}
