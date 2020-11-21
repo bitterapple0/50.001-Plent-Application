d4d2627bb2d2b2c734b10a712d1cee541b751069
KiranM27 <58102994+KiranM27@users.noreply.github.com>
2020-11-22 02:16:06 +0800

    Modified LoginActivity.java

--- app/src/main/java/com/example/plent/myActivities/LoginActivity.java
+++ app/src/main/java/com/example/plent/myActivities/LoginActivity.java
@@ -11,6 +11,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -25,6 +27,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText email;
     private EditText password;
+    Button login;
+    Button createAcc;
     Api api;
     ArrayList<User> userList;
     boolean emailInList = false;
@@ -37,6 +41,9 @@ public class LoginActivity extends AppCompatActivity {
 
         email = findViewById(R.id.emailInput);
         password =  findViewById(R.id.passwordInput);
+        login = findViewById(R.id.login);
+        createAcc = findViewById(R.id.createAcc);
+
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://10.0.2.2:5000/") // Same as the one in the SignUpActivity
@@ -45,28 +52,39 @@ public class LoginActivity extends AppCompatActivity {
 
         api = retrofit.create(Api.class);
 
-        for (User u: userList){
-            if (u.getEmail().toString() == email.getText().toString()) {
-                emailInList = true;
-                if (u.getPassword() == password.getText().toString()) {
-                    Intent intent = new Intent(LoginActivity.this, FindEventsActivity.class);
-                    startActivity(intent);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                for (User u: userList){
+                    if (u.getEmail().toString() == email.getText().toString()) {
+                        emailInList = true;
+                        if (u.getPassword() == password.getText().toString()) {
+                            Intent intent = new Intent(LoginActivity.this, FindEventsActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "One or more of the entered credentials are incorrect. Please try again",
+                                    Toast.LENGTH_SHORT).show();
+                            email.setText(null);
+                            password.setText(null);
+                        }
+                    }
                 }
-                else {
-                    Toast.makeText(this, "One or more of the entered credentials are incorrect. Please try again",
-                            Toast.LENGTH_SHORT).show();
-                    email.setText(null);
-                    password.setText(null);
+                if (emailInList == false) {
+                    Toast.makeText(LoginActivity.this, "Looks like there is no account related to the entered email. \n" +
+                            "Try using another email address or Create a new account", Toast.LENGTH_SHORT).show();
                 }
             }
-        }
-        if (emailInList == false) {
-            Toast.makeText(this, "Looks like there is no account related to the entered email. \n" +
-                    "Try using another email address or Create a new account", Toast.LENGTH_SHORT).show();
-        }
 
+        });
 
-
+        createAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void userList(){
