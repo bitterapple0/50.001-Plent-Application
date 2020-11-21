5b44897ba5512c5d513220332d2251e7aa358bee
KiranM27 <58102994+KiranM27@users.noreply.github.com>
2020-11-21 20:57:29 +0800

    Added login.xml

---
+++ app/src/main/java/com/example/plent/myActivities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.plent.myActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.plent.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
